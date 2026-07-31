bae254c211d890fceb0491236f2eebe1c5f9f8f9
Bjrr1907Joel <billyxito19joel@gmail.com>
2026-07-31 13:40:27 -0500

    Update tragos.js

--- js/tragos.js
+++ js/tragos.js
@@ -1,25 +1,29 @@
 const drinks = [
-  ["machu-picchu","Machu Picchu"],
-  ["cuba-libre","Cuba Libre"],
-  ["blue-hawaiian","Blue Hawaiian"],
-  ["margarita","Margarita"],
-  ["blue-margarita","Blue Margarita"],
-  ["pina-colada","Piña Colada"],
-  ["daiquiri","Daiquiri"],
-  ["mojito","Mojito"],
-  ["caipirinha","Caipirinha"],
-  ["laguna-azul","Laguna Azul"],
-  ["chilcano","Chilcano"],
-  ["mojito-frutos-rojos","Mojito de frutos rojos"],
-  ["chilcano-maracuya","Chilcano de maracuyá"],
-  ["pisco-sour","Pisco Sour"]
+  ["machu-picchu", "Machu Picchu"],
+  ["cuba-libre", "Cuba Libre"],
+  ["blue-hawaiian", "Blue Hawaiian"],
+  ["margarita", "Margarita"],
+  ["blue-margarita", "Blue Margarita"],
+  ["pina-colada", "Piña Colada"],
+  ["daiquiri", "Daiquiri"],
+  ["mojito", "Mojito"],
+  ["caipirinha", "Caipirinha"],
+  ["laguna-azul", "Laguna Azul"],
+  ["chilcano", "Chilcano"],
+  ["mojito-frutos-rojos", "Mojito de frutos rojos"],
+  ["chilcano-maracuya", "Chilcano de maracuyá"],
+  ["pisco-sour", "Pisco Sour"]
 ];
 
-function renderDrinks(){
+function renderDrinks() {
   const grid = document.getElementById("drinkGrid");
-  grid.innerHTML = drinks.map(([slug,name]) => `
+
+  grid.innerHTML = drinks.map(([slug, name]) => `
     <article class="drink">
-      <img src="img/tragos/${slug}.png" alt="${name}" loading="lazy">
+      <img src="img/tragos/${slug}.png"
+           alt="${name}"
+           loading="lazy">
+
       <strong>${name}</strong>
     </article>
   `).join("");
