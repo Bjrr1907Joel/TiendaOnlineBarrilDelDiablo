b89ff39c6bd3422365373ff6ff18689b2c366293
Bjrr1907Joel <billyxito19joel@gmail.com>
2026-07-31 13:52:52 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,19 @@
-const CACHE_NAME = "barril-diablo-v1";
+const CACHE_NAME = "barril-diablo-v4";
+
 const ASSETS = [
   "./",
   "./index.html",
+  "./tienda.html",
   "./css/estilos.css",
   "./css/animaciones.css",
+  "./css/tienda.css",
   "./js/app.js",
   "./js/pagos.js",
   "./js/redes.js",
   "./js/delivery.js",
   "./js/tragos.js",
+  "./js/productos.js",
+  "./js/tienda.js",
   "./img/logo.png",
   "./img/qr_pago.jpeg",
   "./img/yape.png",
@@ -16,11 +21,39 @@ const ASSETS = [
 ];
 
 self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, copy);
+        });
+
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
